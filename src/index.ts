f258b3ffb147834e5e48a6154836fdcb57cf0916
matheusarb <mat.araujoribeiro@gmail.com>
2022-11-16 20:20:26 -0300

    add todo list logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-// import { v4 as uuidV4 } from 'uuid';
-
-// console.log(uuidV4());
+import { v4 as uuidV4 } from 'uuid';
 
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector<HTMLFormElement>('#new-task-form');
-const task = document.querySelector('#new-task-file');
+const form = document.getElementById('#new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-file');
+
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null ) return
+
+  const task = {
+    id: ,
+    title: ,
+    completed: false,
+    createdAt: new Date()
+  }
+  
+  input.value
+})
